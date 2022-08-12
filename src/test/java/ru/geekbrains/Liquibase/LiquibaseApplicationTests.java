c9b4bf0c706b3926f86ca51cf6aa2bd346b0f83e
Fokusmod <Info@mail.ru>
2022-08-13 02:16:23 +0300

    Initial commit

---
+++ src/test/java/ru/geekbrains/Liquibase/LiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.Liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
